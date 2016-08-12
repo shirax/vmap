a50527deaa7ef20dae39b07b98a8615a8960dc61
Yuval Adam <yuval@y3xz.com>
2016-08-12 20:27:16 +0300

    Implement basic full screen Map component

---
+++ webapp/imports/ui/Map.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+    componentDidMount () {
+      mapboxgl.accessToken = 'pk.eyJ1IjoieXV2YWRtIiwiYSI6ImNpaWN5b3Q0cTAwMGZ3ZmtyeXFkcjQwcmYifQ.OgVsYPOhzLUOOD82004IJw';
+      this.map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9'
+      });
+    }
+
+  render() {
+     return (
+       <div id='map' style={{width: 100+'vw', height: 100+'vh'}}></div>
+    );
+  }
+
+}
