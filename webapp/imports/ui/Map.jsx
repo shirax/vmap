adc229a068454c108887552179c0a6a813e9ec50
Yuval Adam <yuval@y3xz.com>
2016-08-12 20:28:48 +0300

    Fix indentation

--- webapp/imports/ui/Map.jsx
+++ webapp/imports/ui/Map.jsx
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
 
-    componentDidMount () {
-      mapboxgl.accessToken = 'pk.eyJ1IjoieXV2YWRtIiwiYSI6ImNpaWN5b3Q0cTAwMGZ3ZmtyeXFkcjQwcmYifQ.OgVsYPOhzLUOOD82004IJw';
-      this.map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v9'
-      });
-    }
+  componentDidMount () {
+    mapboxgl.accessToken = 'pk.eyJ1IjoieXV2YWRtIiwiYSI6ImNpaWN5b3Q0cTAwMGZ3ZmtyeXFkcjQwcmYifQ.OgVsYPOhzLUOOD82004IJw';
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v9'
+    });
+  }
 
   render() {
      return (
