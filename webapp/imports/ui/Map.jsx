7ed41652fc12949c52005a84b6a816f64d8c4824
Yuval Adam <yuval@y3xz.com>
2016-08-12 22:41:29 +0300

    Add sample marker

--- webapp/imports/ui/Map.jsx
+++ webapp/imports/ui/Map.jsx
@@ -10,6 +10,38 @@ export default class Map extends Component {
       center: [34.8, 31.2],
       zoom: 6
     });
+
+    var geojson = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "properties": {
+                    "message": "Foo",
+                    "icon": "office"
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        35, 32
+                    ]
+                }
+            }
+        ]
+    }
+
+    geojson.features.map((marker) => {
+        var el = document.createElement('i');
+        el.className = 'marker';
+        el.style.backgroundImage = 'url(/img/' + marker.properties.icon + '.svg)';
+        el.style.width = 40 + 'px';
+        el.style.height = 40 + 'px';
+
+        new mapboxgl.Marker(el)
+            .setLngLat(marker.geometry.coordinates)
+            .addTo(this.map);
+    });
+
   }
 
   componentWillUnmount () {
