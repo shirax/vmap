724d0e7acee02a1775292df3ef6442289db3b94e
Shira H <shira65@gmail.com>
2016-08-13 10:54:42 +0300

    emphasize hasbara aspects

--- webapp/imports/ui/App.jsx
+++ webapp/imports/ui/App.jsx
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Task from './Task.jsx';
+import { Tasks } from '../api/tasks.js';
 
-import Map from './Map.jsx';
-
+// App component - represents the whole app
 export default class App extends Component {
+
+  handleSubmit(event){
+    event.preventDefault();
+    const place = ReactDOM.findDOMNode(this.refs.place).value.trim();
+    const info = ReactDOM.findDOMNode(this.refs.info).value.trim();
+    Tasks.insert({
+      place: place,
+      info: info
+    });
+  }
 
   render() {
     return (
-      <Map/>
+      <div className="container">
+        <header>
+          <h1>Location Details</h1>
+        </header>
+        <form className="new-task">
+          <input
+            type="text"
+            ref="place"
+            placeholder="Type to add a new place"
+          />
+          <br/>
+          <input
+            type="text"
+            ref="info"
+            placeholder="Type additional info"
+          />
+         <br/>
+         <button onClick={this.handleSubmit.bind(this)}>
+              click me
+         </button>
+        </form>
+      </div>
     );
   }
-
 }
